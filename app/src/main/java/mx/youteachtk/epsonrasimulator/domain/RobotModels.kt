a29485d9e1038763f891cee4b0eb708e95da651e
youteachtk <youteach.tk@gmail.com>
2026-09-15 13:35:47 -0600

    domain: add robot and pose models

---
+++ app/src/main/java/mx/youteachtk/epsonrasimulator/domain/RobotModels.kt
@@ -0,0 +1,40 @@
+package mx.youteachtk.epsonrasimulator.domain
+
+data class JointDefinition(
+    val id: String,
+    val displayName: String,
+    val type: JointType,
+    val minValue: Double,
+    val maxValue: Double
+)
+
+enum class JointType {
+    REVOLUTE,
+    PRISMATIC
+}
+
+data class RobotDefinition(
+    val id: String,
+    val displayName: String,
+    val joints: List<JointDefinition>,
+    val modelAsset: String? = null
+)
+
+data class JointState(
+    val values: List<Double>
+)
+
+data class CartesianPose(
+    val x: Double,
+    val y: Double,
+    val z: Double,
+    val rx: Double = 0.0,
+    val ry: Double = 0.0,
+    val rz: Double = 0.0
+)
+
+data class TeachPoint(
+    val name: String,
+    val pose: CartesianPose,
+    val preferredJointState: JointState? = null
+)
