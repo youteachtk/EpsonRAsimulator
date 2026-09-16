8b09d9ab10e445b708410b64a61be4d4a948c99e
youteachtk <youteach.tk@gmail.com>
2026-09-16 17:40:25 -0600

    feat: expose source-capable SPEL adapter

--- app/src/main/java/mx/youteachtk/epsonrasimulator/adapters/ProgrammingLanguageAdapter.kt
+++ app/src/main/java/mx/youteachtk/epsonrasimulator/adapters/ProgrammingLanguageAdapter.kt
@@ -1,6 +1,12 @@
 package mx.youteachtk.epsonrasimulator.adapters
 
+import mx.youteachtk.epsonrasimulator.programming.ProgramDocumentSession
+
 interface ProgrammingLanguageAdapter {
     val id: ProgrammingLanguageAdapterId
     val displayName: String
+}
+
+interface SourceProgrammingLanguageAdapter : ProgrammingLanguageAdapter {
+    fun openSession(sourceText: String): ProgramDocumentSession
 }
